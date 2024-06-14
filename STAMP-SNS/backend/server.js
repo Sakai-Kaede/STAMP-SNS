8e9ccec93f377e94972562477584456541272aa3
Sakai-Kaede <kana1024k@gmail.com>
2024-06-14 23:50:28 +0900

    Adding Routing

--- STAMP-SNS/backend/server.js
+++ STAMP-SNS/backend/server.js
@@ -1,1 +1,6 @@
-console.log('Hello World!!');
+const express = require('express');
+const app = express();
+
+app.use('/api/user', userRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/upload'. uploadRoute);
