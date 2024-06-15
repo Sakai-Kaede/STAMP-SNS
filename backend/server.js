72138d03193f93fb4a8ed3fe56f2c90299c14efb
Sakai-Kaede <kana1024k@gmail.com>
2024-06-16 01:41:08 +0900

    Connecting to the server and configuring routing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
+const userRoute = require('./routes/user');
+const profileRoute = require('./routes/profile');
+const uploadRoute = require('./routes/upload');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const PORT = 5000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('DBと接続');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use('/api/user', userRoute);
 app.use('/api/profile', profileRoute);
-app.use('/api/upload'. uploadRoute);
+app.use('/api/upload', uploadRoute);
+
+app.listen(PORT, () => {
+  console.log('サーバを起動');
+})
