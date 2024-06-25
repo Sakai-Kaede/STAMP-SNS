b12723750c1fd5615fecdb667c53ced21fe0fba0
Sakai-Kaede <kana1024k@gmail.com>
2024-06-25 23:58:09 +0900

    Create a schema for stamps

---
+++ backend/models/Stamp.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const StampSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    xPosition: {
+      type: String,
+      required: true,
+    },
+    yPosition: {
+      type: String,
+      required: true,
+    },
+  }, 
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('stamp', StampSchema);
