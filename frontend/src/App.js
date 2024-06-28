f27b2602b59f69228dcf7f64c067c8f555e475d4
Sakai-Kaede <kana1024k@gmail.com>
2024-06-28 23:56:39 +0900

    Routes set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
