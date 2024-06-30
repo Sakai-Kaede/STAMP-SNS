5c4b3e78d7b5c6cd06cf8bc7f0428c96d50dc580
Sakai-Kaede <kana1024k@gmail.com>
2024-06-30 23:18:16 +0900

    Installed Axios and wrote it in Canvas.jsx

--- frontend/src/components/canvas/Canvas.jsx
+++ frontend/src/components/canvas/Canvas.jsx
@@ -1,23 +1,31 @@
+import { useEffect, useState } from 'react';
 import './Canvas.css'
+import axios from 'axios';
 
 export default function Canvas() {
   const images = [];
 
   document.addEventListener('click', function(event) {
+    const [posts, setPosts] = useState([]);
+
+    // ページが読み込まれたときに、スタンプを読み込む
+    useEffect(() => {
+      const response = axios;
+    }, []);
+
     const x = event.clientX;
     const y = event.clientY;
               
-    if(y >= 70) {
-      const img = document.createElement('img');
-      img.src = 'https://via.placeholder.com/50';
-      img.classList.add('image');
-      img.style.left = `${x - 50}px`;
-      img.style.top = `${y - 50}px`;
 
-      document.body.appendChild(img);
+    const img = document.createElement('img');
+    img.src = 'https://via.placeholder.com/50';
+    img.classList.add('image');
+    img.style.left = `${x - 50}px`;
+    img.style.top = `${y - 50}px`;
+
+    document.body.appendChild(img);
                 
-      images.push({ element: img, x: x, y: y });
-      }
+    images.push({ element: img, x: x, y: y });
   });
 
   return (
