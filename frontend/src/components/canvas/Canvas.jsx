f304a832918f4f8f24270329e412d22cc76e608c
Sakai-Kaede <kana1024k@gmail.com>
2024-07-01 02:05:54 +0900

    A Stamp component was created and added to the canvas component

--- frontend/src/components/canvas/Canvas.jsx
+++ frontend/src/components/canvas/Canvas.jsx
@@ -1,34 +1,47 @@
 import { useEffect, useState } from 'react';
 import './Canvas.css'
+import Stamp from '../stamp/Stamp';
 import axios from 'axios';
 
 export default function Canvas() {
-  const images = [];
+  const [posts, setPosts] = useState([]);
 
-  document.addEventListener('click', function(event) {
-    const [posts, setPosts] = useState([]);
+  // ページが読み込まれたときに、スタンプを読み込む
+  useEffect(() => {
+    // useEffectの無名関数には、asyncがつけられない
+    const fetchPosts = async () => {
+      const response = await axios.get('/posts/668175d1453cb58c7b48ff35');
+      setPosts(response.data);
+    }
+    fetchPosts();
+  }, []);
 
-    // ページが読み込まれたときに、スタンプを読み込む
-    useEffect(() => {
-      const response = axios.get('/posts/');
-    }, []);
+  // const images = [];
 
-    const x = event.clientX;
-    const y = event.clientY;
+  // document.addEventListener('click', function(event) {
+
+  //   const x = event.clientX;
+  //   const y = event.clientY;
               
 
-    const img = document.createElement('img');
-    img.src = 'https://via.placeholder.com/50';
-    img.classList.add('image');
-    img.style.left = `${x - 50}px`;
-    img.style.top = `${y - 50}px`;
+  //   const img = document.createElement('img');
+  //   img.src = 'https://via.placeholder.com/50';
+  //   img.classList.add('image');
+  //   img.style.left = `${x - 50}px`;
+  //   img.style.top = `${y - 50}px`;
 
-    document.body.appendChild(img);
+  //   document.body.appendChild(img);
                 
-    images.push({ element: img, x: x, y: y });
-  });
+  //   images.push({ element: img, x: x, y: y });
+  // });
 
   return (
-    <div className='back'></div>
+    <div className='canvas'>
+      <div className='canvasWrapper'>
+        {posts.map((post) => (
+          <Stamp post={post} key={post.id}/>
+        ))}
+      </div>
+    </div>
   )
 }
