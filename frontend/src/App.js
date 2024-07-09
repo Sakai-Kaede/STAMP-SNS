3d5b969d6196a1723dc0de1d2d45eea0ee6dc774
Sakai-Kaede <kana1024k@gmail.com>
2024-07-09 22:39:04 +0900

    Added configuration page with user update and logout functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Setting from "./pages/setting/Setting";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { AuthContext } from "./state/AuthContext";
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/setting' element={<Setting />}/>
       </Routes>
     </Router>
   );
